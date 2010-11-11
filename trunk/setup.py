034ca2b790336bbe22f6073c7692efe44215f21b
ramezquitao@gmail.com <ramezquitao@gmail.com@52aa3ac8-5221-438b-56b5-b9aa86cceb5b>
2010-11-11 19:17:07 +0000

    First commit

---
+++ trunk/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(name='pyMoCo',
+      version='0.0',
+      description='Python wrapper for motion controller drivers',
+      url='http://code.google.com/p/pymoco/',
+      py_modules = ['standa',]
+      )
