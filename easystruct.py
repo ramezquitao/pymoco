81ba694bcfade481341d738b65f8cfec6f1f4580
ramezquitao@gmail.com <ramezquitao@gmail.com@52aa3ac8-5221-438b-56b5-b9aa86cceb5b>
2011-07-12 21:47:55 +0000

    The driver is almost ready, mode and parameters can be changed

--- easystruct.py
+++ easystruct.py
@@ -1,16 +1,25 @@
-
 import struct
-
 class Easystruct:
-    __structdef__ = [("primero","B",3),("segundo","i",30000)]
     
-    
-    def __init__(self,buf=None,**kwargs):
+    def __init__(self, structdef, buf=None, **kwargs):
+        """
+        structdef a list of tuples containing:
+        (att_name,att_type", def_val, class2buf, buf2class)
+        
+        att_name    String containing the attribute name
+        att_type    String containing the attribute type as defined in the strunct module
+        class2buf   Function to convert the value as represented in the class to buffer representation
+        buf2class   Function to convert the value as represented in the buffer to class representation
+        """
         
         #Create dinamically the attribs, the format string, and fill it
         #with the default values
-        self.fmt=""
-        for attr, tp, defv in self.__class__.__structdef__:
+        
+        self.structdef=structdef
+        
+        self.fmt="="
+        
+        for attr, tp, defv, mod, mod1 in structdef:
             setattr(self,attr,defv)
             self.fmt=self.fmt+tp
             
@@ -30,10 +39,26 @@ class Easystruct:
         Fill the attributes using the values given in buf
         """
         
-        upk=struct.unpack(self.__fmt__,buf)
+        upk=struct.unpack(self.fmt,buf)
         
-        for val,attr in zip(upk,self.__class__.__structdef__):
-            setattr(self,attr[0],val)
-
-a=Easystruct(primero=0)
+        for val,attr in zip(upk,self.structdef):
+            a,tp,defv, mod, mod1 = attr
+            if mod1==None:
+                setattr(self,a,val)
+            else:
+                setattr(self,a,mod1(val))
+    
+    def tobuffer(self):
+        bl=[]
+        for attr, tp, defv, mod, mod1 in self.structdef:
+            if mod== None:
+                bl.append(getattr(self, attr))
+            else:
+                bl.append(mod(getattr(self, attr)))
+                
+        print len(bl),
+        buf=struct.pack(self.fmt, *bl)
+        return buf
+        
+#a=Easystruct(primero=0)
         
